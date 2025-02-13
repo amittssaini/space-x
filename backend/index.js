b0b30b4934bed92ca5b37ebf0b990e59bb23df03
amittssaini <amitssaini999@gmail.com>
2025-02-13 20:03:57 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors')
 const spaceRouter = require('./routes/space.route')
 const PORT = process.env.PORT;
 
 const app = express();
 app.listen(PORT,()=>console.log(`port is listen at ${PORT}`))
-
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/space',spaceRouter);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,58 @@
 
 import './App.css';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
   const [latestLaunch,setLatestLaunch] =  useState({})
+  const [totalPastLaunches,setTotalPastLaunches]=useState(0)
 
   const fetchData=async()=>{
-    let url ;
+    let url ="http://localhost:8082/api/space";
     try {
+    let response = await axios.get(`${url}/latest`)
+    console.log(response.data)
+    setLatestLaunch(response.data);
+    console.log(latestLaunch)
+    } catch (error) {
+      
+    }
+  }
+
+  const fetchPastLaunches=async()=>{
+    let url ="http://localhost:8082/api/space";
+    try {
+    let response = await axios.get(`${url}/pastLaunches`)
+    console.log(response.data)
+    setTotalPastLaunches(response.data.TotalPastLaunches)
+  
     
     } catch (error) {
       
     }
   }
+  useEffect(()=>{
+    fetchData();
+    fetchPastLaunches();
+  },[])
   return (
     <div className="App">
-     
+     <div className='App-header'> Space -x  Launch Info </div>
+     <div className='launch-container'>
+     <div className='launch-card'>
+     <h2>Latest Launch</h2>
+     <p><strong>Launch Name:</strong> {latestLaunch['Launch Name']}</p>
+      <p><strong>Launch Date (UTC):</strong> {latestLaunch['Launch Date (UTC)']}</p>
+      <p><strong>Rocket Name:</strong> {latestLaunch['Rocket Name']}</p>
+      <p><strong>LaunchPad Name:</strong> {latestLaunch['LaunchPad Name']}</p>
+      </div>
+            <div className='launch-card'>
+              <h2>Total Past Launches </h2>
+              <p>
+              {totalPastLaunches!==0?totalPastLaunches:'...Loading'}
+              </p>
+            </div>
+      </div>
     </div>
   );
 }
