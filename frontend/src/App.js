a318a7d3c26332c05a5612d3267bd9c72fc53c0b
amittssaini <amitssaini999@gmail.com>
2025-02-13 23:06:37 +0530

    frontend app change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,34 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios'
-
+const endpoint={
+  url : "https://space-x-l99u.onrender.com/api/space"
+}
 function App() {
   const [latestLaunch,setLatestLaunch] =  useState({})
   const [totalPastLaunches,setTotalPastLaunches]=useState(0)
 
   const fetchData=async()=>{
-    let url ="http://localhost:8082/api/space";
+    
     try {
-    let response = await axios.get(`${url}/latest`)
+    let response = await axios.get(`${endpoint.url}/latest`)
     console.log(response.data)
     setLatestLaunch(response.data);
     console.log(latestLaunch)
     } catch (error) {
-      
+      console.log("Error is occured during fetching the latest launch data ",error)
     }
   }
 
   const fetchPastLaunches=async()=>{
-    let url ="http://localhost:8082/api/space";
     try {
-    let response = await axios.get(`${url}/pastLaunches`)
+    let response = await axios.get(`${endpoint.url}/pastLaunches`)
     console.log(response.data)
     setTotalPastLaunches(response.data.TotalPastLaunches)
   
     
     } catch (error) {
-      
+      console.log("Error is occured during fetching the Past launch data ",error)
     }
   }
   useEffect(()=>{
@@ -41,14 +42,14 @@ function App() {
      <div className='launch-container'>
      <div className='launch-card'>
      <h2>Latest Launch</h2>
-     <p><strong>Launch Name:</strong> {latestLaunch['Launch Name']}</p>
-      <p><strong>Launch Date (UTC):</strong> {latestLaunch['Launch Date (UTC)']}</p>
-      <p><strong>Rocket Name:</strong> {latestLaunch['Rocket Name']}</p>
-      <p><strong>LaunchPad Name:</strong> {latestLaunch['LaunchPad Name']}</p>
+     <p><strong>Launch Name:</strong> {latestLaunch['Launch Name']|| '...Loading'}</p>
+      <p><strong>Launch Date (UTC):</strong> {latestLaunch['Launch Date (UTC)']|| '...Loading'}</p>
+      <p><strong>Rocket Name:</strong> {latestLaunch['Rocket Name'] || '...Loading'}</p>
+      <p><strong>LaunchPad Name:</strong> {latestLaunch['LaunchPad Name']|| '...Loading'}</p>
       </div>
             <div className='launch-card'>
               <h2>Total Past Launches </h2>
-              <p>
+              <p> <strong>Total No. of launches in the past </strong>
               {totalPastLaunches!==0?totalPastLaunches:'...Loading'}
               </p>
             </div>
