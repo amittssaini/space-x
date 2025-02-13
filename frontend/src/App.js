353d561377aad4e2a613f78da00a26ce8f6105c8
amittssaini <amitssaini999@gmail.com>
2025-02-13 11:13:48 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  const [latestLaunch,setLatestLaunch] =  useState({})
+
+  const fetchData=async()=>{
+    let url ;
+    try {
+    
+    } catch (error) {
+      
+    }
+  }
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
